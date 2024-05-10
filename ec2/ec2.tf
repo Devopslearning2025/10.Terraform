495773789c9dd230688aa8df5174e110206ef304
devops <dev0pslearning2025@gmail.com>
2024-05-10 14:52:16 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -9,28 +9,28 @@ resource "aws_instance" "test_instance" {
     Name = "test_instance"
     createdby = "Ramesh vallapu"
   }
-}
 
 resource "aws_security_group" "allow_ssh" {
-        name = "allow_ssh"
-        description = "allowing the ssh access "
+    name = "allow_ssh"
+    description = "allowing the ssh access "
 
-        ingress {
-            from_port        = 22
-            to_port          = 22
-            protocol         = "tcp"
-            cidr_blocks      = ["0.0.0.0/0"]
-        }
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
         
-        egress {
-            from_port        = 0 #0 to 0 means, opening all protocol 
-            to_port          = 0
-            protocol         = "-1"
-            cidr_blocks      = ["0.0.0.0/0"]
-        }
-         tags = {
-            Name = "allow_ssh"
-            createdBy = "Ramesh Vallapu"
-        }
-}
+    egress {
+        from_port        = 0 #0 to 0 means, opening all protocol 
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 
+    tags = {
+        Name = "allow_ssh"
+        createdBy = "Ramesh Vallapu"
+    }
+}
+}
